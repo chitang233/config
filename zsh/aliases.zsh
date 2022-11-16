5358bee77e12f8179ce743ea0df0508dcd58df1b
Chi_Tang <chitang@duck.com>
2022-11-16 19:48:49 +0800

    Initial commit

---
+++ zsh/aliases.zsh
@@ -0,0 +1,8 @@
+alias rm='trash'
+alias c="clear"
+alias gg='git clone --depth 1'
+alias ga='git add'
+alias gc='git commit'
+alias gP='git push'
+alias gp='git pull'
+alias tree='exa --tree --icons --color=always'
